dafaf7377f389b0f67dd2ca81615769be50db5d6
Kenn Pascascio <kenn370@gmail.com>
2016-10-03 11:20:35 -0400

    This is how we print hello world to the console

---
+++ src/_001_HelloWorld/HelloWorld.java
@@ -0,0 +1,31 @@
+package _001_HelloWorld;
+
+/**
+ * Created by kpascascio on 10/3/16.
+ *
+ * This class prints out Hello World
+ */
+public class HelloWorld {
+
+    public static void main(String args[]){
+        System.out.println("Hello World!");
+        System.out.print("Hello Kenn");
+
+//      CHALLENGE: Have the students print their favorite joke to the console.
+//      Joke:
+        System.out.println("I needed a password eight characters long, so I picked Snow White and the Seven Dwarfs");
+
+/*      CHALLENGE: Why Don't these work?
+            System.in.println("Hello World");
+            system.out.println("Hello, World!");
+            System.out.printLn("Hello Class")
+            System.out.println("Hello, Nurse!);
+*/
+        System.out.println("Hello World");
+        System.out.println("Hello, World!");
+        System.out.println("Hello Class");
+        System.out.println("Hello, Nurse!");
+    }
+
+
+}
